2f4c6217c42694a7482e19bd37170a8aa2d5fc13
Player-5 <57650225+Player-5@users.noreply.github.com>
2021-11-28 20:32:12 -0500

    Updated Licence Notices
    Each source files licence notice has been updated to better comply with
    GPL convention.

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@
 #    A simple installation script for auto-update
 #    Copyright (C) 2021  Player-5
 
-#    This program is free software: you can redistribute it and/or modify
+#    auto-update is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 
-#    This program is distributed in the hope that it will be useful,
+#    auto-update is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 
 #    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#    along with auto-update.  If not, see <https://www.gnu.org/licenses/>.
 
 # Checks whether the user has the permissions needed to install the script.
 if [[ "$(whoami)" != root ]]; then
